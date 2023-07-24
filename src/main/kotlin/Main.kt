50764afd0c467371bf13c5b59e689c9eb3d4fd7f
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-07-24 14:02:50 -0300

    polimorfismo entre contas diferentes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,11 @@
 fun main() {
     println("Bem vindo ao Kotlin Bank")
-//    comportamentoConta()
+    // comportamentoConta()
     //testeFuncionario()
-
-    val contaCorrente = ContaCorrente(titular = "natan", numeroConta = 1000)
-    val contaPolpanca = ContaPolpanca(titular = "alberto", numeroConta = 1001)
-
-    contaCorrente.depositar(200.0)
-    contaPolpanca.depositar(200.0)
-
-    contaCorrente.sacar(100.0)
-    contaPolpanca.sacar(100.0)
-
-    contaCorrente.transferir(contaDestino = contaPolpanca, valorDeTranferencia = 50.0)
-    contaPolpanca.minhaConta()
-    contaCorrente.minhaConta()
+    //testaContasDiferentes()
 }
+
+
 
 
 
---
+++ src/main/kotlin/testaContasDiferentes.kt
@@ -0,0 +1,14 @@
+fun testaContasDiferentes() {
+    val contaCorrente = ContaCorrente(titular = "natan", numeroConta = 1000)
+    val contaPolpanca = ContaPolpanca(titular = "alberto", numeroConta = 1001)
+
+    contaCorrente.depositar(200.0)
+    contaPolpanca.depositar(200.0)
+
+    contaCorrente.sacar(100.0)
+    contaPolpanca.sacar(100.0)
+
+    contaCorrente.transferir(contaDestino = contaPolpanca, valorDeTranferencia = 50.0)
+    contaPolpanca.minhaConta()
+    contaCorrente.minhaConta()
+}
