da29945aa3328fb8d398ebcfbe18c7c21d72be8a
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-07 17:36:23 -0300

    Alterando uma Array

--- src/main/kotlin/br/com/alura/kotlin_bank/Main.kt
+++ src/main/kotlin/br/com/alura/kotlin_bank/Main.kt
@@ -11,9 +11,28 @@ import java.lang.IllegalStateException
 
 fun main() {
 
-    val salarios:DoubleArray = doubleArrayOf(1500.00, 2300.00, 5000.00, 3500.00)
+    val salarios: DoubleArray = doubleArrayOf(1500.00, 2300.00, 5000.00, 3500.00)
 
+    val aumento = 1.1
+    var indece = 0
 
+    for (salario in salarios) {
+        salarios[indece] = salario * aumento
+        indece++
+    }
+    println(salarios.contentToString())//contentToString() Retorna uma representação de string do conteúdo da matriz especificada como se fosse
+    println()
+
+    for (indece in salarios.indices) {
+        salarios[indece] = salarios[indece] * aumento
+    }
+    println(salarios.contentToString())
+    println()
+
+    salarios.forEachIndexed() { i, salario -> //forEachIndexed()Executa a ação determinada em cada caractere, fornecendo índice sequencial com o caractere.
+        salarios[i] = salario * aumento
+    }
+    println(salarios.contentToString())
 }
 
 //arrays
