b5e0ebf4dee73a8b3b5f32c20b751d7b0aa0af69
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-07 10:09:11 -0300

    safe call

--- src/main/kotlin/br/com/alura/kotlin_bank/Main.kt
+++ src/main/kotlin/br/com/alura/kotlin_bank/Main.kt
@@ -8,9 +8,15 @@ import br.com.alura.kotlin_bank.teste.*
 
 fun main() {
 var enderecoNull:Endereco?=null// ? indica que pode ser null
+var enderecoNullValido:Endereco?= Endereco(logradouro = "casa")// ? indica que pode ser null
 //    endereco!!.logradouro//! ! não deixa ele ser null
-    val enderecoNaoNull:Endereco=enderecoNull!!
-    enderecoNaoNull.logradouro
+//    val enderecoNaoNull:Endereco=enderecoNull!!
+//    enderecoNaoNull.logradouro
+    println(enderecoNull?.logradouro)//usando safe call ? ele verifica e retorna se ele for null ou não
+    println(enderecoNullValido?.logradouro)//usando safe call ? ele verifica e retorna se ele for null ou não
+    //usando o safe call você tem que garantir que o valor é null ou nao exemplo
+    println(enderecoNullValido?.logradouro?.length?.toByte())//verifico que cada etapa é ou nao um null
+
 }
 
 
