ea29ea426777f3a104b46479c51d24de71d002ca
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-07 18:07:53 -0300

    renges

--- src/main/kotlin/br/com/alura/kotlin_bank/Main.kt
+++ src/main/kotlin/br/com/alura/kotlin_bank/Main.kt
@@ -1,6 +1,7 @@
 package br.com.alura.kotlin_bank
 
 import br.com.alura.kotlin_bank.Array.ArrayforEach
+import br.com.alura.kotlin_bank.Array.alterandoArray
 import br.com.alura.kotlin_bank.Array.navegandoArray
 import br.com.alura.kotlin_bank.exception.SaldoInsuficienteException
 import br.com.alura.kotlin_bank.modelo.*
@@ -11,34 +12,45 @@ import java.lang.IllegalStateException
 
 fun main() {
 
-    val salarios: DoubleArray = doubleArrayOf(1500.00, 2300.00, 5000.00, 3500.00)
-
-    val aumento = 1.1
-    var indece = 0
-
-    for (salario in salarios) {
-        salarios[indece] = salario * aumento
-        indece++
+    //doc https://kotlinlang.org/docs/basic-syntax.html#ranges
+    val sequencia:IntRange = 1.rangeTo(10)
+    for (s in sequencia){
+        println("$s")
     }
-    println(salarios.contentToString())//contentToString() Retorna uma representação de string do conteúdo da matriz especificada como se fosse
     println()
 
-    for (indece in salarios.indices) {
-        salarios[indece] = salarios[indece] * aumento
+   val numeroPares: IntProgression = 0..100 step 2
+    for (numeropar in numeroPares){
+        println("$numeropar")
     }
-    println(salarios.contentToString())
     println()
 
-    salarios.forEachIndexed() { i, salario -> //forEachIndexed()Executa a ação determinada em cada caractere, fornecendo índice sequencial com o caractere.
-        salarios[i] = salario * aumento
+    var numerosDecrescentePar: IntProgression =100 downTo 0 step 2
+    numerosDecrescentePar.forEach {
+        println("$it")
     }
-    println(salarios.contentToString())
+    println()
+
+    val intervalo: ClosedFloatingPointRange<Double> = 1500.0..5000.0
+    val salario=35000.0
+    if (salario in intervalo){
+        println("O salario esta entre o intervalo ")
+    }else{
+        println("não salario esta entre o intervalo ")
+    }
+
+    val alfabeto: ClosedRange<String> ="a".."z"
+    val letra="k"
+    println(letra in alfabeto)
+
 }
+
+
 
 //arrays
 //navegandoArray()
 //ArrayforEach()
-
+//alterandoArray()
 
 // println("Bem vindo ao Kotlin Bank")
 // comportamentoConta()
